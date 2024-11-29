eb43c4cca8713f7bdf13a8c818d821abe488d7f6
Emmanuel-Mbewe <emmanuelmbewe180@gmail.com>
2024-11-29 16:31:26 +0000

    I have changed a graphic-designer page

--- src/pages/graphic-designer.js
+++ src/pages/graphic-designer.js
@@ -32,6 +32,11 @@ const GraphicDesignerPortfolio = () => {
       title: 'Tutorial Poster',
       description: 'An informative poster for an online tutorial series.',
     },
+    {
+      src: '/images/designs/Tutorial Poster.png',
+      title: 'Tutorial Poster',
+      description: 'An informative poster for an online tutorial series.',
+    },
   ];
 
   const [selectedImage, setSelectedImage] = useState(null);
