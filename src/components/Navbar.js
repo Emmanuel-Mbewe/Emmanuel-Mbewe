9557501d3839662a489e3478cc02e69366fe672c
Emmanuel-Mbewe <emmanuelmbewe180@gmail.com>
2025-02-12 21:14:38 -0800

    Worked on the NavBar component: Added projects links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,55 +1,58 @@
-"use client"
+"use client";
 
 // components/Navbar.js
 import { useState } from "react";
 import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
+import Image from "next/image";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [projectsOpen, setProjectsOpen] = useState(false);
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
 
   return (
-    <nav className="bg-white fixed w-full z-50">
+    <nav className="bg-white fixed w-full z-50 shadow-md">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-center h-16">
-          <div className="flex items-center space-x-8">
-            <div className="flex-shrink-0">
-              <a href="/" className="heading text-black font-bold text-xl">Emmanuel Binalison Mbewe</a>
-            </div>
-
-            <div className="hidden md:flex space-x-8">
-              <a href="/" className="text-black hover:text-gray-700">Home</a>
-              <a href="about" className="text-black hover:text-gray-700">About</a>
-              <a href="blog" className="text-black hover:text-gray-700">Blog</a>
-              <a href="contact" className="text-black hover:text-gray-700">Contact</a>
-              <a href="cv" className="text-black hover:text-gray-700">CV</a>
-            </div>
-
-            <div className="hidden md:flex items-center space-x-4">
-              {/* <div className="relative">
-                <input
-                  type="text"
-                  placeholder="Search..."
-                  className="px-4 py-2 rounded-full bg-gray-200 text-black focus:outline-none"
-                />
-              </div> */}
-
-              <div className="flex space-x-3 text-black">
-                <a href="https://web.facebook.com/profile.php?id=100095368848132"><FaFacebook className="hover:text-gray-700" /></a>
-                <a href="#"><FaTwitter className="hover:text-gray-700" /></a>
-                <a href="#"><FaInstagram className="hover:text-gray-700" /></a>
-              </div>
-            </div>
+        <div className="flex items-center justify-between h-16">
+          <div className="flex items-center space-x-4">
+            {/* Logo and Name */}
+            <Image src="/images/Emma.jpg" alt="Logo" width={40} height={40} className="rounded-full" />
+            <a href="/" className="text-black font-bold text-xl">Emmanuel</a>
           </div>
 
-          <div className="md:hidden flex items-center absolute right-4">
-            <button
-              className="text-black focus:outline-none"
-              onClick={toggleMenu}
-            >
+          {/* Desktop Menu */}
+          <div className="hidden md:flex space-x-8">
+            <a href="/" className="text-black hover:text-gray-700">Home</a>
+            <a href="/about" className="text-black hover:text-gray-700">About</a>
+            <a href="/cv" className="text-black hover:text-gray-700">CV</a>
+            <div className="relative" onMouseEnter={() => setProjectsOpen(true)} onMouseLeave={() => setProjectsOpen(false)}>
+              <a href="#" className="text-black hover:text-gray-700">Projects</a>
+              {projectsOpen && (
+                <div className="absolute left-0 mt-2 w-48 bg-white shadow-lg rounded-md py-2">
+                  <a href="/projects/graphic-design" className="block px-4 py-2 text-black hover:bg-gray-100">Graphic Design</a>
+                  <a href="/projects/web" className="block px-4 py-2 text-black hover:bg-gray-100">Web</a>
+                  <a href="/projects/mobile-app" className="block px-4 py-2 text-black hover:bg-gray-100">Mobile App</a>
+                  <a href="/projects/teaching-education" className="block px-4 py-2 text-black hover:bg-gray-100">Teaching and Education</a>
+                </div>
+              )}
+            </div>
+            <a href="/contact" className="text-black hover:text-gray-700">Contact</a>
+            <a href="/blog" className="text-black hover:text-gray-700">Blog</a>
+          </div>
+
+          {/* Social Media Links */}
+          <div className="hidden md:flex items-center space-x-4">
+            <a href="https://web.facebook.com/profile.php?id=100095368848132"><FaFacebook className="hover:text-gray-700 text-black" /></a>
+            <a href="#"><FaTwitter className="hover:text-gray-700 text-black" /></a>
+            <a href="#"><FaInstagram className="hover:text-gray-700 text-black" /></a>
+          </div>
+
+          {/* Mobile Menu Button */}
+          <div className="md:hidden flex items-center">
+            <button className="text-black focus:outline-none" onClick={toggleMenu}>
               <svg
                 className="w-6 h-6"
                 fill="none"
@@ -69,19 +72,22 @@ const Navbar = () => {
         </div>
       </div>
 
+      {/* Mobile Menu */}
       {isOpen && (
         <div className="md:hidden">
           <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
             <a href="/" className="block text-black hover:text-gray-700">Home</a>
-            <a href="about" className="block text-black hover:text-gray-700">About</a>
-            <a href="blog" className="block text-black hover:text-gray-700">Blog</a>
-            <a href="contact" className="block text-black hover:text-gray-700">Contact</a>
-            <a href="cv" className="text-black hover:text-gray-700">cv</a>
+            <a href="/about" className="block text-black hover:text-gray-700">About</a>
+            
+            <a href="/cv" className="block text-black hover:text-gray-700">CV</a>
+            <a href="/projects" className="block text-black hover:text-gray-700">Projects</a>
             <div className="flex justify-center mt-4 space-x-3">
               <a href="https://web.facebook.com/profile.php?id=100095368848132"><FaFacebook className="hover:text-gray-700 text-black" /></a>
               <a href="#"><FaTwitter className="hover:text-gray-700 text-black" /></a>
               <a href="#"><FaInstagram className="hover:text-gray-700 text-black" /></a>
             </div>
+            <a href="/contact" className="block text-black hover:text-gray-700">Contact</a>
+            <a href="/blog" className="block text-black hover:text-gray-700">Blog</a>
           </div>
         </div>
       )}
